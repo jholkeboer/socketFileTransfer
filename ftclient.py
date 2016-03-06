bb5548c6be08ac79321e69a10c9972ffcdc3333e
jholkeboer <jholkeboer@users.noreply.github.com>
2016-03-06 13:05:34 -0800

    basic client

--- ftclient.py
+++ ftclient.py
@@ -4,3 +4,107 @@
 # Project 2
 # holkeboj@onid.oregonstate.edu
 # ftclient.py
+# Based on Python Docs: https://docs.python.org/2/howto/sockets.html
+
+import sys
+import socket
+import os
+
+# check for argument
+if len(sys.argv) not in [5,6]:
+    print "Usage: python chatclient.py [server host] [server port] [command] [data port] [filename]"
+    sys.exit()
+
+host = sys.argv[1]
+control_port = int(sys.argv[2])
+command = sys.argv[3]
+data_port = int(sys.argv[4])
+if command == "-g":
+    if len(sys.argv) == 6:
+        filename = sys.argv[5]
+    else:
+        print "Error: Please provide a filename."
+        sys.exit()
+
+print 'Welcome to the socket file transfer client.'
+
+
+# create socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    # get host and connect socket
+    host_ip = socket.gethostbyname(host)
+    sock.connect((host_ip, control_port))
+    
+    if command in ['-l','-g']:
+        sock.send("%s\t\n" % command)
+        if command == '-l':
+            sock.send("%s\t%s\n" % (command, data_port))
+        elif command == '-g':
+            sock.send("%s\t%s\t%s\n" % (command, filename, data_port))
+    else:
+        sock.send("%s\t\n" % command)
+
+    
+    
+    response = sock.recv(1024)
+    if response != "ERROR\n":
+        recv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        recv_socket.bind((host_ip, data_port))
+        recv_socket.listen(5)
+        (ftserver, address) = recv_socket.accept()
+        print "Connected to server."
+        
+        if command = '-l':
+            file_list = ''
+            data = recv_socket.recv(1024)
+            while data:
+                file_list = file_list + data
+                data = recv_socket.recv(1024)
+            print file_list
+        else if command = '-g':
+            data = recv_socket.recv(1024)
+            with open(filename, 'w') as f:
+                while data:
+                    if data = ("File not found.\n"):
+                        print data
+                        break
+                    else:
+                        f.write(data)
+                        data = recv_socket.recv(1024)
+        
+        ftserver.close()
+    else:
+        print "Invalid command.  Exiting..."
+        sock.shutdown
+        sock.close()
+        sys.exit()
+            
+
+    # while (msg != '\quit'):
+    #     # send message
+    #     while len(msg) > 500:
+    #         print 'Your message was too long. Maximum 500 characters.'
+    #         msg = raw_input("%s> " % handle)
+    #     sock.send("%s> %s" % (handle,msg))
+            
+    #     # wait to receive message
+    #     print '[waiting for response...]'
+    #     response = sock.recv(512)
+    #     if "\quit" in response:
+    #         print "Received quit signal. Closing connection."
+    #         sys.exit()
+    #     print response
+
+    #     # input new message to send
+    #     msg = raw_input("%s> " % handle)
+    
+    # react to \quit message
+    print "Closing Connection."
+    
+    # send \quit to server
+    sock.send("\quit")
+except socket.gaierror:
+    print 'Could not find host. Exiting.'
+    sys.exit()
